b947394e8b578321fc59e1793af7fbeaa4a3d07f
Gautam <gautamgs20012008@gmail.com>
2025-03-02 11:25:14 +0530

    Front End Complete API Integration Left

---
+++ index.js
@@ -0,0 +1,7 @@
+{/* <i class="fas fa-sun"></i>  <!-- Sunny -->
+<i class="fas fa-cloud"></i>  <!-- Cloudy -->
+<i class="fas fa-cloud-showers-heavy"></i>  <!-- Rainy -->
+<i class="fas fa-bolt"></i>  <!-- Thunderstorm -->
+<i class="fas fa-snowflake"></i>  <!-- Snowy -->
+<i class="fas fa-smog"></i>  <!-- Foggy -->
+ */}
